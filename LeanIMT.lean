6a0af75cbb0e3d6b0ff695755785e959650fc264
JSMassmann <sylvie@83c.nl>
2025-06-23 23:20:22 +0100

    Initialize project, formalize Jen71 Prop. 1.1(a)-(d)

---
+++ LeanIMT.lean
@@ -0,0 +1,9 @@
+/-
+This formalizes many theorems of inner model theory (IMT), starting from basic properties of L
+(but using the J-hierarchy throughout) up to models with Woodin cardinals.
+
+Bibliography:
+- Jen71 = Ronald Jensen, The Fine Structure of the Constructible Hierarchy
+-/
+
+import LeanIMT.RudFunc
